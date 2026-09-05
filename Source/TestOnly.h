8ca70a01074b5830ce1e8f1724c48b79235ac6e2
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2026-09-05 17:12:04 +0900

    [Update] 필요없는 태그 제거

--- Source/TestOnly.h
+++ Source/TestOnly.h
@@ -6,25 +6,4 @@ namespace TLGameplayTags
 {	
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look_Mouse);
-
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_SwordAndShield_NormalAttack);
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_Bow_NormalShot);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_Bow_MultiShot);
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_ThrowingKnife);
-	
-
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_SwordAndShield_NormalAttack);
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_Bow_NormalShot);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_Bow_MultiShot);
-	
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Skill_ThrowingKnife);
-
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Damage);
-
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Attack_SpawnProjectile);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Attack_Nocked);
-	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayEvent_Attack_MeleeHit);
 }
